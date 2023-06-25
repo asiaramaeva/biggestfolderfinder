13545df92f74a631baec720ec18839cf076aa097
Asia Ramaeva <ramaevaasia@yandex.ru>
2023-06-25 18:28:12 +0300

    div

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String folderPath = "C:/Folder";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+
+        }
+        public static long getFolderSize(File folder)
+        {
+            if(folder.isFile()){
+                return folder.length();
+            }
+            long sum = 0;
+            File[] files = folder.listFiles();
+            for (File file : files)
+            {
+                sum += getFolderSize(file);
+            }
+            return  sum;
+        }
     }
-}
